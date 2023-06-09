3055a2fd57d9090d25c36a19b6f3d6bec047b4ec
Shahiduzzaman Tauhid <shahiduzzamantauhid@gmail.com>
2023-06-10 03:10:52 +0600

    imported express cors

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express()
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.eaxagba.mongodb.net/?retryWrites=true&w=majority`;
+// respond with "hello world" when a GET request is made to the homepage
