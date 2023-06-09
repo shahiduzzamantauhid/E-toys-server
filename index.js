d80caa15ec934b2e2e0ad5f5f906d45284544acc
Shahiduzzaman Tauhid <shahiduzzamantauhid@gmail.com>
2023-06-10 03:13:55 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -16,3 +16,98 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('hello Tauhid')
 })
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const toyCollection = client.db("etoys").collection("toymarket_etoys");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  } finally {
+    // // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})  
