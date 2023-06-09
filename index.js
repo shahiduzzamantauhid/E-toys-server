8d9a896e1665b2d008a44b4a8eff37eb597bcf30
Shahiduzzaman Tauhid <shahiduzzamantauhid@gmail.com>
2023-06-10 03:11:53 +0600

    inithial response setup

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.eaxagba.mongodb.net/?retryWrites=true&w=majority`;
 // respond with "hello world" when a GET request is made to the homepage
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('hello Tauhid')
+})
